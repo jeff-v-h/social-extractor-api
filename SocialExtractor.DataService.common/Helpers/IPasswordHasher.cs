71c0f7cd60dcaf45cd86c0e2fe6bfe569c6016ca
Jeffrey Huang <jeffvh@outlook.com>
2019-12-22 17:10:16 +0000

    add common layer

---
+++ SocialExtractor.DataService.common/Helpers/IPasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using SocialExtractor.DataService.common.Models;
+
+namespace SocialExtractor.DataService.common.Helpers
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        PasswordCheck Check(string hash, string password);
+    }
+}
---
+++ SocialExtractor.DataService.common/Models/PasswordCheck.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialExtractor.DataService.common.Models
+{
+    public class PasswordCheck
+    {
+        public bool Verified { get; set; }
+        public bool NeedsUpgrade { get; set; }
+    }
+}
